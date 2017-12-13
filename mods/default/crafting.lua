11c90324324d2eac7115f55ece4a489d0d963606
Wuzzy <almikes@aol.com>
2017-12-13 23:57:23 +0100

    Remove nyan cats

--- mods/default/crafting.lua
+++ mods/default/crafting.lua
@@ -1113,20 +1113,6 @@ minetest.register_craft({
 
 minetest.register_craft({
 	type = "fuel",
-	recipe = "default:nyancat",
-	burntime = 1,
-})
-
-
-minetest.register_craft({
-	type = "fuel",
-	recipe = "default:nyancat_rainbow",
-	burntime = 1,
-})
-
-
-minetest.register_craft({
-	type = "fuel",
 	recipe = "default:sapling",
 	burntime = 10,
 })
