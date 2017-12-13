c05173843071676bd8e17d5879443c92bc422458
Wuzzy <almikes@aol.com>
2017-12-13 23:10:13 +0100

    Remove scorched stuff

--- mods/default/craftitems.lua
+++ mods/default/craftitems.lua
@@ -103,12 +103,6 @@ minetest.register_craftitem("default:clay_brick", {
 })
 
 
-minetest.register_craftitem("default:scorched_stuff", {
-	description = "Scorched Stuff",
-	inventory_image = "default_scorched_stuff.png",
-})
-
-
 minetest.register_craftitem("default:obsidian_shard", {
 	description = "Obsidian Shard",
 	inventory_image = "default_obsidian_shard.png",
