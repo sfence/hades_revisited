1fe63c02628891afbdb3c49f8441600adff09b2b
Wuzzy <almikes@aol.com>
2017-12-19 23:28:30 +0100

    Add dripping water

---
+++ mods/drippingwater/init.lua
@@ -0,0 +1,58 @@
+--Dripping Water Mod
+--by kddekadenz
+
+-- License of code, textures & sounds: CC0
+
+--Drop entities
+
+minetest.register_entity("drippingwater:drop_water", {
+	hp_max = 2,
+	physical = true,
+	collide_with_objects = false,
+	collisionbox = {0,0,0,0,0,0},
+	visual = "cube",
+	visual_size = {x=0.05, y=0.1},
+	textures = {"default_water.png","default_water.png","default_water.png","default_water.png", "default_water.png","default_water.png"},
+	spritediv = {x=1, y=1},
+	initial_sprite_basepos = {x=0, y=0},
+
+	on_activate = function(self, staticdata)
+		self.object:setsprite({x=0,y=0}, 1, 1, true)
+		self.object:set_armor_groups({immortal=1})
+	end,
+
+	on_step = function(self, dtime)
+		local k = math.random(1,222)
+		local ownpos = self.object:getpos()
+
+		if k==1 then
+			self.object:setacceleration({x=0, y=-5, z=0})
+		end
+
+		if minetest.get_node({x=ownpos.x, y=ownpos.y+0.5, z=ownpos.z}).name == "air" then
+			self.object:setacceleration({x=0, y=-5, z=0})
+		end
+	
+		if minetest.get_node({x=ownpos.x, y=ownpos.y -0.5, z=ownpos.z}).name ~= "air" then
+			self.object:remove()
+			minetest.sound_play({name="drippingwater_drip"}, {pos = ownpos, gain = 0.5, max_hear_distance = 8})
+		end
+	end,
+})
+
+--Create drop
+
+minetest.register_abm({
+	nodenames = {"group:crumbly", "group:stone"},
+	neighbors = {"group:water"},
+        interval = 2,
+        chance = 22,
+        action = function(pos)
+		if minetest.get_node({x=pos.x, y=pos.y-1, z=pos.z}).name == "air" and 
+				minetest.get_node({x=pos.x, y=pos.y-2, z=pos.z}).name == "air" then
+			local i = math.random(-45,45) / 100
+			minetest.add_entity({x=pos.x + i, y=pos.y-0.501, z=pos.z + i}, "drippingwater:drop_water")
+		end
+        end,
+})
+
