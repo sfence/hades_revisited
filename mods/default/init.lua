88aa3aa479a3d3dc18c145b00120f86c135dc400
Wuzzy <almikes@aol.com>
2017-12-23 02:51:42 +0100

    Change intro message

--- mods/default/init.lua
+++ mods/default/init.lua
@@ -54,11 +54,18 @@ dofile(minetest.get_modpath("default").."/furnace.lua")
 
 --infotext
 minetest.register_on_newplayer(function(player)
+	-- TODO: Replace beginner message by something other than chat
 	local cb = function(player)
-		minetest.chat_send_player(player:get_player_name(), "You have been deployed on the planet Hades.")
-		minetest.chat_send_player(player:get_player_name(), "Search for some water. Lay down fertile sand next to the water and it will soon turn into dirt. Use the crafting guide to see what you can craft.")
-		minetest.chat_send_player(player:get_player_name(), "Your mission is to terraform this place and build a nice place to live in.")
-		minetest.chat_send_player(player:get_player_name(), "Good luck! <END OF TRANSMISSION>")
+		if minetest.settings:get_bool("creative_mode") then
+			minetest.chat_send_player(player:get_player_name(), "Welcome to planet Hades!")
+			minetest.chat_send_player(player:get_player_name(), "Creative Mode is active. You have unlimited supplies.")
+			minetest.chat_send_player(player:get_player_name(), "Create the buildings of your dreams and have fun! <END OF TRANSMISSION>")
+		else
+			minetest.chat_send_player(player:get_player_name(), "You have stranded on planet Hades.")
+			minetest.chat_send_player(player:get_player_name(), "Search for some water. Lay down fertile sand next to the water and it will soon turn into dirt. Use the crafting guide to see what you can craft.")
+			minetest.chat_send_player(player:get_player_name(), "Survive and build a nice habitable place!")
+			minetest.chat_send_player(player:get_player_name(), "Good luck! <END OF TRANSMISSION>")
+		end
 	end
 	minetest.after(2.0, cb, player)
 end)
