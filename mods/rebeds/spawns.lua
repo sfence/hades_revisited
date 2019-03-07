6b5c96ce658eb33cf15aca643617c7fb052f854c
Wuzzy <Wuzzy2@mail.ru>
2019-03-07 15:00:42 +0100

    Replace legacy player meta functions

--- mods/rebeds/spawns.lua
+++ mods/rebeds/spawns.lua
@@ -1,12 +1,12 @@
 function rebeds.set_spawn(player, pos)
 	local name = player:get_player_name()
-	player:set_attribute("rebeds:spawn_pos", minetest.pos_to_string(pos))
+	player:get_meta():set_string("rebeds:spawn_pos", minetest.pos_to_string(pos))
 	minetest.chat_send_player(name, "Spawn position set!")
 end
 
 -- Returns the spawn pos of player, or nil if none is set.
 function rebeds.get_spawn(player)
-	local s = player:get_attribute("rebeds:spawn_pos")
+	local s = player:get_meta():get_string("rebeds:spawn_pos")
 	local spos = minetest.string_to_pos(s)
 	if spos ~= nil and spos ~= "" then
 		return spos
