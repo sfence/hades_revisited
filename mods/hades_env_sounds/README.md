253bfdb02e384c68983f6a18493599b740f67af1
Wuzzy <Wuzzy2@mail.ru>
2020-03-25 14:52:22 +0100

    Play environmental sounds near liquids

---
+++ mods/hades_env_sounds/README.md
@@ -0,0 +1,24 @@
+## Credits
+### Mod
+This mod is based on `env_sounds` from Minetest Game.
+
+The code is licensed under the MIT License.
+
+### Sounds
+
+* `hades_env_sounds_flowing_lava.*.ogg`:
+    * Sound based on sound by Blender Foundation
+    * License: CC BY 3.0
+    * Source: https://opengameart.org/content/lava-loop
+* `hades_env_sounds_lava_source.*.ogg`:
+    * Sound based on sound by freesound.org user Fission9
+    * License: CC0
+    * Source: https://freesound.org/people/Fission9/sounds/474851/
+* `env_sounds_water.*.ogg
+    * Sound by Yuval
+    * License: CC0
+    * Source: https://freesound.org/people/Yuval/sounds/197023/
+
+## Licenses
+* CC0: https://creativecommons.org/publicdomain/zero/1.0/
+* CC BY 3.0: https://creativecommons.org/licenses/by/3.0/
---
+++ mods/hades_env_sounds/init.lua
@@ -0,0 +1,71 @@
+-- Parameters
+
+local radius = 8 -- Node search radius around player
+
+local around_nodes = {
+	{ nodes = {"default:water_flowing"}, sound = "env_sounds_water" },
+	{ nodes = {"default:lava_flowing"}, sound = "hades_env_sounds_flowing_lava" },
+	{ nodes = {"default:lava_source"}, sound = "hades_env_sounds_lava_source" },
+}
+
+-- End of parameters
+
+-- Update sound for player
+
+local function update_sound(player)
+	local player_name = player:get_player_name()
+	local ppos = player:get_pos()
+	local areamin = vector.subtract(ppos, radius)
+	local areamax = vector.add(ppos, radius)
+
+	for a=1, #around_nodes do
+		local nodes = around_nodes[a].nodes
+		local wpos, _ = minetest.find_nodes_in_area(areamin, areamax, nodes)
+		local total = #wpos
+		-- Find average position
+		local wposav = vector.new()
+		for _, pos in ipairs(wpos) do
+			wposav.x = wposav.x + pos.x
+			wposav.y = wposav.y + pos.y
+			wposav.z = wposav.z + pos.z
+		end
+		wposav = vector.divide(wposav, total)
+
+		minetest.sound_play(
+			around_nodes[a].sound,
+			{
+				pos = wposav,
+				to_player = player_name,
+				gain = math.min(0.04 + total * 0.004, 0.4),
+			}
+		)
+	end
+end
+
+
+-- Update sound 'on joinplayer'
+
+minetest.register_on_joinplayer(function(player)
+	update_sound(player)
+end)
+
+minetest.register_on_respawnplayer(function(player)
+	minetest.after(0.1, function(player_name)
+		local player = minetest.get_player_by_name(player_name)
+		if player then
+			update_sound(player)
+		end
+	end, player:get_player_name())
+end)
+
+
+-- Cyclic sound update
+
+local function cyclic_update()
+	for _, player in pairs(minetest.get_connected_players()) do
+		update_sound(player)
+	end
+	minetest.after(3.5, cyclic_update)
+end
+
+minetest.register_on_mods_loaded(cyclic_update)
