09735c7661e43be66b8a3c0535d43fd247d470fb
Wuzzy <Wuzzy2@mail.ru>
2020-03-29 03:50:55 +0200

    Default: Move atteched lamp to deco.lua

--- mods/default/simple_nodes.lua
+++ mods/default/simple_nodes.lua
@@ -814,29 +814,6 @@ minetest.register_node("default:lava_source", {
 	sounds = hades_sounds.node_sound_lava_defaults(),
 })
 
-
--- Lamps
-minetest.register_node("default:lamp_wall", {
-    description = "Attached Lamp",
-	drawtype = "nodebox",
-	paramtype = "light",
-	paramtype2 = "wallmounted",
-	tiles = {"default_lamp.png"},
-	wield_image = "default_lamp_inv.png",
-	inventory_image = "default_lamp_inv.png",
-	sunlight_propagates = true,
-	light_source = 13,
-	walkable = false,
-	groups = {snappy=2,cracky=3,oddly_breakable_by_hand=3,attached_node=1},
-	node_box = {
-		type = "wallmounted",
-		wall_side = {-0.5, -0.1875, -0.1875, -0.375, 0.1875, 0.1875},
-		wall_top = {-0.25, 0.375, -0.25, 0.25, 0.5, 0.25},
-		wall_bottom = {-0.25, -0.5, -0.25, 0.25, -0.375, 0.25},
-	},
-	sounds = hades_sounds.node_sound_glass_defaults(),
-})
-
 minetest.register_node("default:cobble", {
 	description = "Cobblestone",
 	tiles = {"default_cobble.png"},
