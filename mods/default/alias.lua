22f639d4891ca2e4614a7f67c62ce007055a6f01
Wuzzy <Wuzzy2@mail.ru>
2020-03-29 04:01:58 +0200

    Default: Remove alias.lua

--- mods/default/alias.lua
+++
@@ -1,3 +0,0 @@
-minetest.register_alias("default:mese_block", "default:mese")
-minetest.register_alias("default:nyancat", "default:diamondblock")
-minetest.register_alias("default:nyancat_rainbow", "default:goldblock")
