177e5a53fa45e2069026c1b5ddd7d68db5e45172
Wuzzy <Wuzzy2@mail.ru>
2020-03-29 03:07:43 +0200

    Move leafdecay code to hades_trees

--- mods/hades_trees/init.lua
+++ mods/hades_trees/init.lua
@@ -10,4 +10,4 @@ dofile(minetest.get_modpath("hades_trees").."/jungle.lua")
 dofile(minetest.get_modpath("hades_trees").."/cjtree.lua")
 dofile(minetest.get_modpath("hades_trees").."/wood.lua")
 dofile(minetest.get_modpath("hades_trees").."/crafting.lua")
-
+dofile(minetest.get_modpath("hades_trees").."/leafdecay.lua")
