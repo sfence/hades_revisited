161ce32fb9c69de8ab4bd2e0aa9665fc185c0c61
Wuzzy <Wuzzy2@mail.ru>
2020-07-11 16:28:52 +0200

    Remove custom item code, fall back to default behavior
    The old code was too complicated and it broke in 5.3.0.

---
+++ mods/item_tweaks/README.md
@@ -0,0 +1,3 @@
+This mod tweaks item entities. Currently, it only plays an item drop sound.
+
+This mod and all sound files are licenced under Creative Commons Zero (CC0).
--- mods/item_tweaks/init.lua
+++ mods/item_tweaks/init.lua
@@ -1,3 +1,11 @@
-local modpath = minetest.get_modpath(minetest.get_current_modname())
+local old_item_drop = minetest.item_drop
 
-dofile(modpath.."/item_drop.lua")
+function minetest.item_drop(itemstack, dropper, pos)
+	minetest.sound_play("item_drop", {
+		pos=pos,
+		gain = 1.0,
+		max_hear_distance = 32,
+	}, true)
+	return old_item_drop(itemstack, dropper, pos)
+end
+
