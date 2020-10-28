6c8687441e601b1bfea0a00f28999e339e9c272b
Wuzzy <Wuzzy2@mail.ru>
2020-10-28 23:00:32 +0100

    Change default mob of mob spawner

--- mods/mobs/spawner.lua
+++ mods/mobs/spawner.lua
@@ -4,7 +4,7 @@ local S = minetest.get_translator("mobs")
 
 -- mob spawner
 
-local spawner_default = "mobs_animal:pumba 10 15 0 0"
+local spawner_default = "mobs_hades:mutant 0 15 0 0"
 
 minetest.register_node("mobs:spawner", {
 	tiles = {"mob_spawner.png"},
