44592aff4172e148a57700e04b9bee1432f7b02b
Wuzzy <Wuzzy2@mail.ru>
2020-11-06 11:53:53 +0100

    Tweak selbox of toilet

--- mods/3dforniture/nodes.lua
+++ mods/3dforniture/nodes.lua
@@ -229,8 +229,8 @@ local S = minetest.get_translator("3dforniture")
 		{ description = S("Toilet"),
 		tiles = {
 			"forniture_marble.png", "forniture_marble.png",
-			"forniture_marble_s1.png", "forniture_marble_s1.png",
-			"forniture_marble_s2.png", "forniture_marble_s2.png",
+			"forniture_marble_sb1.png", "forniture_marble_sb1.png",
+			"forniture_marble_sb2.png", "forniture_marble_sb2.png",
 		},
 		drawtype = "nodebox",
 		sunlight_propagates = false,
@@ -244,9 +244,24 @@ local S = minetest.get_translator("3dforniture")
 				{-0.3,-0.2,-0.3, 0.3,0,0.35},
 				{-0.25,0,-0.25, 0.25,0.05,0.25},
 				{-0.3,0,0.3, 0.3,0.4,0.5},
-				{-0.05,0.4,0.35, 0.05,0.45,0.45},
+				{-0.05,0.4,0.35, 0.05,0.45,0.45}, -- flush button
 			},
 		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.2,-0.5,-0.2, 0.2,-0.45,0.5},
+
+				{-0.1,-0.2,0.35, 0.1,0.0,0.5},
+				{-0.1,-0.45,-0.1, 0.1,-0.2,0.5},
+
+				{-0.3,-0.2,-0.3, 0.3,0,0.35},
+				{-0.25,0,-0.25, 0.25,0.05,0.25},
+				{-0.3,0,0.3, 0.3,0.4,0.5},
+				{-0.05,0.4,0.35, 0.05,0.45,0.45}, -- flush button
+			},
+		},
+
 		drop ="3dforniture:toilet",
 		groups = {cracky=3,},
 		sounds = hades_sounds.node_sound_stone_defaults(),
@@ -281,6 +296,21 @@ local S = minetest.get_translator("3dforniture")
 				{0.2,-0.15,-0.3, 0.3,0,0.1},
 				{-0.25,0,0.2, 0.25,0.5,0.25},
 				{-0.3,0,0.3, 0.3,0.4,0.5},
+				{-0.05,0.4,0.35, 0.05,0.45,0.45}, -- flush button
+			},
+		},
+		selection_box = {
+			type = "fixed",
+			fixed = {
+				{-0.2,-0.5,-0.2, 0.2,-0.45,0.5},
+
+				{-0.1,-0.2,0.35, 0.1,0.0,0.5},
+				{-0.1,-0.45,-0.1, 0.1,-0.2,0.5},
+
+				{-0.3,-0.2,-0.3, 0.3,0,0.35},
+				{-0.3,0,0.3, 0.3,0.4,0.5},
+				{-0.25,0,0.2, 0.25,0.5,0.25},
+				{-0.05,0.4,0.35, 0.05,0.45,0.45}, -- flush button
 			},
 		},
 		drop = "3dforniture:toilet",
