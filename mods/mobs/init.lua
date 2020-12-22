32ded8810ed05c275ebc3f42ab763243231695e6
SFENCE <sfence.software@gmail.com>
2020-12-22 09:41:55 +0100

    Add peaceful_player privilege to mobs API, to prevent auto attack player with this privilege.

--- mods/mobs/init.lua
+++ mods/mobs/init.lua
@@ -1,6 +1,11 @@
 
 local path = minetest.get_modpath("mobs")
 
+-- for sinki update
+if 1 then
+  minetest.register_privilege("peaceful_player", { description = "prevents mobs drived by mobs_redo from attacking player", give_to_singleplayer = false});
+end
+
 -- Mob API
 dofile(path .. "/api.lua")
 
