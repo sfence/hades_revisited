482227402e86cf5f3b4532c6758ce9b67a124aae
Wuzzy <Wuzzy2@mail.ru>
2020-12-27 16:42:44 +0100

    Fix piston local var issue

--- mods/mesecons/mesecons_pistons/init.lua
+++ mods/mesecons/mesecons_pistons/init.lua
@@ -56,10 +56,6 @@ end
 
 local piston_remove_pusher = function(pos, node)
 	local pistonspec = minetest.registered_nodes[node.name].mesecons_piston
-	if pushername == pistonspec.pusher then --make sure there actually is a pusher (for compatibility reasons mainly)
-		return
-	end
-
 	local dir = piston_get_direction(pistonspec.dir, node)
 	local pusherpos = mesecon:addPosRule(pos, dir)
 	local pushername = minetest.get_node(pusherpos).name
