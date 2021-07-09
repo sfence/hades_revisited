b73f73fce3c63055cfd6d715b9fa40d056aa475f
Wuzzy <Wuzzy2@mail.ru>
2021-07-09 17:14:37 +0200

    Tweak Crafting inventory page

--- mods/sfinv/init.lua
+++ mods/sfinv/init.lua
@@ -10,12 +10,12 @@ sfinv.register_page("sfinv:crafting", {
 	title = S("Crafting"),
 	get = function(self, player, context)
 		return sfinv.make_formspec(player, context, [[
-				list[current_player;craft;1.75,0.5;3,3;]
-				list[current_player;craftpreview;5.75,1.5;1,1;]
-				image[4.75,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
+				list[current_player;craft;3,0.5;3,3;]
+				image[6,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]
+				list[current_player;craftpreview;7,1.5;1,1;]
 				listring[current_player;main]
 				listring[current_player;craft]
-				image_button[6.75,1.5;1,1;craftguide_book.png;__craftguide;]
+				image_button[8,1.5;1,1;craftguide_book.png;__craftguide;]
 				tooltip[__craftguide;]]..F(S("Crafting guide"))..[[]
 			]], true)
 	end
