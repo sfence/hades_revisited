c0724e19fcf9ac0963102adcac585958d81d96f2
Wuzzy <Wuzzy2@mail.ru>
2021-07-09 17:10:26 +0200

    Add back the craftguide button to survival GUI

--- mods/hades_gui/init.lua
+++ mods/hades_gui/init.lua
@@ -1,3 +1,6 @@
+local S = minetest.get_translator("hades_gui")
+local F = minetest.formspec_escape
+
 hades_gui = {}
 
 hades_gui.gui_inventory_bg_img = "background9[0,0;8,8.5;hades_gui_inventory.png;true;3]"
@@ -6,8 +9,10 @@ hades_gui.gui_survival_form = "size[10,8.5]"..
 	hades_gui.gui_inventory_bg_img..
 	"list[current_player;main;0,4.25;10,1;]"..
 	"list[current_player;main;0,5.5;10,3;10]"..
-	"list[current_player;craft;4,0.5;3,3;]"..
-	"list[current_player;craftpreview;8,1.5;1,1;]"..
-	"image[7,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]]"..
+	"list[current_player;craft;3,0.5;3,3;]"..
+	"image[6,1.5;1,1;gui_furnace_arrow_bg.png^[transformR270]]"..
+	"list[current_player;craftpreview;7,1.5;1,1;]"..
+	"image_button[8,1.5;1,1;craftguide_book.png;__craftguide;]"..
+	"tooltip[__craftguide;"..F(S("Crafting guide")).."]"..
 	"listring[current_player;main]"..
 	"listring[current_player;craft]"
