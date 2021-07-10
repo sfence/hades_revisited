d4e1c289ca42a004af2a1979ead80acf07a06abe
Wuzzy <Wuzzy2@mail.ru>
2021-07-10 22:56:00 +0200

    Simplify straight steel bars

--- mods/hades_furniture/nodes.lua
+++ mods/hades_furniture/nodes.lua
@@ -486,82 +486,46 @@ local S = minetest.get_translator("hades_furniture")
 
 --[[ ----- Medieval theme -----]]
 
-	-- Steel Bars
-	minetest.register_node("hades_furniture:bars",
-		{ description = S("Steel Bars"),
+	-- Steel Bars (only straight section)
+	minetest.register_node("hades_furniture:bars", {
+		description = S("Straight Steel Bars"),
 		tiles = {
-			"hades_furniture_black_metal.png",
-			"hades_furniture_black_metal.png",
 			"hades_furniture_black_metal_s1.png",
 			"hades_furniture_black_metal_s1.png",
-			"hades_furniture_black_metal_s2.png",
-			"hades_furniture_black_metal_s2.png",
 		},
 		drawtype = "nodebox",
 		sunlight_propagates = true,
 		paramtype = 'light',
+		paramtype2 = 'facedir',
 		is_ground_content = false,
-		connects_to = {"hades_furniture:bars", "hades_furniture:L_binding_bars", "hades_fences:fence_rusty", "hades_core:steelblock"},
-		connect_sides = { "left", "right", "front", "back" },
 		node_box = {
-			type = "connected",
+			type = "fixed",
 			fixed = {
 				{-0.1,-0.5,-0.1, 0.1,0.5,0.1}, -- center bar
-			},
-			connect_right = {
-				{0.4,-0.5,-0.1, 0.5,0.5,0.1}, -- bar
-				{0.1,-0.5,-0.05, 0.4,-0.45,0.05}, -- low rail
-				{0.1,0.45,-0.05, 0.4,0.5,0.05}, -- high rail
-			},
-			connect_left = {
-				{-0.5,-0.5,-0.1, -0.4,0.5,0.1}, -- bar
-				{-0.4,-0.5,-0.05, -0.1,-0.45,0.05}, -- low rail
-				{-0.4,0.45,-0.05, -0.1,0.5,0.05}, -- high rail
-			},
-			connect_front = {
-				{-0.1,-0.5,-0.5,0.1,0.5,-0.4}, -- bar
-				{-0.05,-0.5,-0.4, 0.05,-0.45,-0.1}, -- low rail
-				{-0.05,0.45,-0.4, 0.05,0.5,-0.1}, -- high rail
-			},
-			connect_back = {
-				{-0.1,-0.5,0.4,0.1,0.5,0.5}, -- bar
-				{-0.05,-0.5,0.1, 0.05,-0.45,0.4}, -- low rail
-				{-0.05,0.45,0.1, 0.05,0.5,0.4}, -- high rail
+				{0.4,-0.5,-0.1, 0.5,0.5,0.1}, -- right bar
+				{0.1,-0.5,-0.05, 0.4,-0.45,0.05}, -- right low rail
+				{0.1,0.45,-0.05, 0.4,0.5,0.05}, -- right high rail
+				{-0.5,-0.5,-0.1, -0.4,0.5,0.1}, -- left bar
+				{-0.4,-0.5,-0.05, -0.1,-0.45,0.05}, -- left low rail
+				{-0.4,0.45,-0.05, -0.1,0.5,0.05}, -- left high rail
 			},
 		},
 		collision_box = {
-			type = "connected",
-			fixed = {
-				{-0.15,-0.5,-0.15, 0.15,0.5,0.15}, -- center bar
-			},
-			connect_right = {
-				{0.15,-0.5,-0.15, 0.5,0.5,0.15},
-			},
-			connect_left = {
-				{-0.5,-0.5,-0.15, -0.15,0.5,0.15},
-			},
-			connect_back = {
-				{-0.15,-0.5,-0.15, 0.15,0.5,0.5},
-			},
-			connect_front = {
-				{-0.15,-0.5,-0.5, 0.15,0.5,-0.15},
-			},
-
+			type = "fixed",
+			fixed = {-0.5,-0.5,-0.15, 0.5,0.5,0.15},
 		},
 		groups = {cracky=1},
 		sounds = hades_sounds.node_sound_metal_defaults(),
+		on_rotate = "simple",
 	})
 
-	-- Binding bars (center bar is thicker than for the normal steel bars)
+	-- Binding steel bars (for corners and junctions).
+	-- Also, center bar is slightly thicker
 	minetest.register_node("hades_furniture:L_binding_bars",
 		{ description =S("Binding Steel Bars"),
 		tiles = {
-			"hades_furniture_black_metal.png",
-			"hades_furniture_black_metal.png",
 			"hades_furniture_black_metal_s1.png",
 			"hades_furniture_black_metal_s1.png",
-			"hades_furniture_black_metal_s2.png",
-			"hades_furniture_black_metal_s2.png",
 		},
 		drawtype = "nodebox",
 		sunlight_propagates = true,
