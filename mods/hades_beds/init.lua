fd1472f58571317604bedb75343491da933590f7
Wuzzy <Wuzzy2@mail.ru>
2021-08-14 14:25:10 +0200

    Remove unused beds formspec

--- mods/hades_beds/init.lua
+++ mods/hades_beds/init.lua
@@ -5,10 +5,6 @@ hades_beds.player = {}
 hades_beds.pos = {}
 hades_beds.spawn = {}
 
-hades_beds.formspec = "size[8,15;true]"..
-		"bgcolor[#080808BB; true]"..
-		"button_exit[2,12;4,0.75;leave;"..minetest.formspec_escape(S("Leave Bed")).."]"
-
 local modpath = minetest.get_modpath("hades_beds")
 
 -- load files
