1596e8f39308fc485771b9e68fdf857092c05980
SFENCE <sfence.software@gmail.com>
2021-08-16 18:40:02 +0200

    Update pipeworks to commit 59c40d9 from https://github.com/sfence/pipeworks .

--- mods/pipeworks/crafts.lua
+++ mods/pipeworks/crafts.lua
@@ -10,6 +10,13 @@ minetest.register_craft( {
 })
 
 minetest.register_craft( {
+        output = "pipeworks:straight_pipe_empty 3",
+        recipe = {
+                { "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty", "pipeworks:pipe_1_empty" },
+        },
+})
+
+minetest.register_craft( {
         output = "pipeworks:spigot 3",
         recipe = {
                 { "pipeworks:pipe_1_empty", "" },
@@ -31,7 +38,7 @@ minetest.register_craft( {
 minetest.register_craft( {
         output = "pipeworks:pump_off",
         recipe = {
-                { "hades_materials:silicon", "hades_materials:silicon", "hades_materials:silicon" },
+                { "hades_core:steel_ingot", "mesecons_solarpanel:solar_panel_off", "hades_core:steel_ingot" },
                 { "group:stick", "hades_core:emerald", "group:stick" },
                 { "hades_core:steel_ingot", "hades_core:steel_ingot", "hades_core:steel_ingot" }
         },
@@ -67,7 +74,7 @@ minetest.register_craft( {
 minetest.register_craft( {
         output = "pipeworks:flow_sensor_empty 2",
         recipe = {
-                { "pipeworks:pipe_1_empty", "hades_core:mese_crystal_fragment", "pipeworks:pipe_1_empty" },
+                { "pipeworks:pipe_1_empty", "mesecons:mesecon", "pipeworks:pipe_1_empty" },
         },
 })
 
@@ -78,158 +85,3 @@ minetest.register_craft( {
 				{ "pipeworks:pipe_1_empty" }
         },
 })
-
-
-minetest.register_craft( {
-	output = "pipeworks:one_way_tube 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "group:stick", "hades_core:mese_crystal", "hades_materials:plastic_sheeting" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-
-minetest.register_craft( {
-	output = "pipeworks:tube_1 6",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "", "", "" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:mese_tube_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "", "hades_core:mese_crystal", "" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	type = "shapeless",
-	output = "pipeworks:mese_tube_000000",
-	recipe = {
-	    "pipeworks:tube_1",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment"
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:conductor_tube_off_1 6",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:mese_crystal_fragment", "hades_core:mese_crystal_fragment", "hades_core:mese_crystal_fragment" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:detector_tube_off_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:mese_crystal_fragment", "hades_materials:silicon", "hades_core:mese_crystal_fragment" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:accelerator_tube_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:mese_crystal_fragment", "hades_core:steel_ingot", "hades_core:mese_crystal_fragment" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:teleport_tube_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:stone_baked", "hades_materials:teleporter_device", "hades_core:stone_baked" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:sand_tube_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:volcanic_sand", "hades_core:volcanic_sand", "hades_core:volcanic_sand" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:sand_tube_1",
-	recipe = {
-	        { "hades_core:volcanic_sand", "pipeworks:tube_1", "hades_core:volcanic_sand" },
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:mese_sand_tube_1 2",
-	recipe = {
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" },
-	        { "hades_core:volcanic_sand", "hades_core:mese_crystal", "hades_core:volcanic_sand" },
-	        { "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:crossing_tube_1 5",
-	recipe = {
-	        { "", "pipeworks:tube_1", "" },
-	        { "pipeworks:tube_1", "pipeworks:tube_1", "pipeworks:tube_1" },
-	        { "", "pipeworks:tube_1", "" }
-	},
-})
-
-
-minetest.register_craft( {
-	type = "shapeless",
-	output = "pipeworks:mese_sand_tube_1",
-	recipe = {
-  "pipeworks:sand_tube_1",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment",
-		"hades_core:mese_crystal_fragment"
-	},
-})
-
--- Various ancillary tube devices
-
-minetest.register_craft( {
-	output = "pipeworks:filter 2",
-	recipe = {
-	        { "hades_core:steel_ingot", "hades_core:steel_ingot", "hades_materials:plastic_sheeting" },
-	        { "group:stick", "hades_core:sapphire", "hades_materials:plastic_sheeting" },
-	        { "hades_core:steel_ingot", "hades_core:steel_ingot", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:mese_filter 2",
-	recipe = {
-	        { "hades_core:steel_ingot", "hades_core:steel_ingot", "hades_materials:plastic_sheeting" },
-	        { "group:stick", "hades_core:mese", "hades_materials:plastic_sheeting" },
-	        { "hades_core:steel_ingot", "hades_core:steel_ingot", "hades_materials:plastic_sheeting" }
-	},
-})
-
-minetest.register_craft( {
-	output = "pipeworks:autocrafter 2",
-	recipe = {
-	        { "hades_core:steel_ingot", "hades_core:bronze_ingot", "hades_core:steel_ingot" },
-	        { "hades_materials:plastic_sheeting", "hades_core:prismatic_gem", "hades_materials:plastic_sheeting" },
-	        { "hades_core:steel_ingot", "hades_core:bronze_ingot", "hades_core:steel_ingot" }
-	},
-})
-
-
